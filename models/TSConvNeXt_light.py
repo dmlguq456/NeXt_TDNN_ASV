a5a486d8116183b22164ac4399abc5f1c789d365
Ui-Hyeop Shin <41767172+dmlguq456@users.noreply.github.com>
2023-12-27 09:49:04 +0900

    Update TSConvNeXt_light.py

--- models/TSConvNeXt_light.py
+++ models/TSConvNeXt_light.py
@@ -57,86 +57,86 @@ class TSConvNeXt_light(nn.Module):
 
 
 
-class NeXtTDNN_Light(nn.Module): #
-    """ NeXt-TDNN-Light model.
+# class NeXtTDNN_Light(nn.Module): #
+#     """ NeXt-TDNN-Light model.
         
-    Args:
-        in_chans (int): Number of input channels. Default: 80
-        depths (tuple(int)): Number of blocks at each stage. Default: [1, 1, 1]
-        dims (int): Feature dimension at each stage. Default: [256, 256, 256]
-        drop_path_rate (float): Stochastic depth rate. Default: 0.
-    """
-    def __init__(self, in_chans=80, # in_channels: 3 -> 80
-                 depths=[1, 1, 1], dims=[256, 256, 256],  
-                 drop_path_rate=0., # 
-                 kernel_size = [7], # 
-                 ):
-        super().__init__()
-        self.depths = depths
-        self.stem = nn.ModuleList() 
-        Conv1DLayer = nn.Sequential( 
-            nn.Conv1d(in_chans, dims[0], kernel_size=4), 
-            LayerNorm(dims[0], eps=1e-6, data_format="channels_first") # ⚡
-        )
-        self.stem.append(Conv1DLayer)
+#     Args:
+#         in_chans (int): Number of input channels. Default: 80
+#         depths (tuple(int)): Number of blocks at each stage. Default: [1, 1, 1]
+#         dims (int): Feature dimension at each stage. Default: [256, 256, 256]
+#         drop_path_rate (float): Stochastic depth rate. Default: 0.
+#     """
+#     def __init__(self, in_chans=80, # in_channels: 3 -> 80
+#                  depths=[1, 1, 1], dims=[256, 256, 256],  
+#                  drop_path_rate=0., # 
+#                  kernel_size = [7], # 
+#                  ):
+#         super().__init__()
+#         self.depths = depths
+#         self.stem = nn.ModuleList() 
+#         Conv1DLayer = nn.Sequential( 
+#             nn.Conv1d(in_chans, dims[0], kernel_size=4), 
+#             LayerNorm(dims[0], eps=1e-6, data_format="channels_first") # ⚡
+#         )
+#         self.stem.append(Conv1DLayer)
 
 
-        self.stages = nn.ModuleList() # 3 feature resolution stages, each consisting of multiple residual blocks
-        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
-        cur = 0
-        for i in range(len(self.depths)): # ⚡
-            stage = nn.Sequential(
-                *[TSConvNeXt_light(dim=dims[i], drop_path=dp_rates[cur + j], kernel_size=kernel_size) for j in range(depths[i])] # ⚡
-            )
-            self.stages.append(stage)
-            cur += depths[i]
+#         self.stages = nn.ModuleList() # 3 feature resolution stages, each consisting of multiple residual blocks
+#         dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))] 
+#         cur = 0
+#         for i in range(len(self.depths)): # ⚡
+#             stage = nn.Sequential(
+#                 *[TSConvNeXt_light(dim=dims[i], drop_path=dp_rates[cur + j], kernel_size=kernel_size) for j in range(depths[i])] # ⚡
+#             )
+#             self.stages.append(stage)
+#             cur += depths[i]
 
-        self.apply(self._init_weights)
+#         self.apply(self._init_weights)
 
-        # MFA layer
-        self.MFA = nn.Sequential( # ⚡
-            nn.Conv1d(3*dims[-1], int(3*dims[-1]), kernel_size=1),
-            LayerNorm(int(3*dims[-1]), eps=1e-6, data_format="channels_first") # ⚡
-        )
+#         # MFA layer
+#         self.MFA = nn.Sequential( # ⚡
+#             nn.Conv1d(3*dims[-1], int(3*dims[-1]), kernel_size=1),
+#             LayerNorm(int(3*dims[-1]), eps=1e-6, data_format="channels_first") # ⚡
+#         )
 
-    def _init_weights(self, m):
-        if isinstance(m, (nn.Conv1d, nn.Linear)): # ⚡
-            trunc_normal_(m.weight, std=.02)
-            nn.init.constant_(m.bias, 0)
+#     def _init_weights(self, m):
+#         if isinstance(m, (nn.Conv1d, nn.Linear)): # ⚡
+#             trunc_normal_(m.weight, std=.02)
+#             nn.init.constant_(m.bias, 0)
 
 
-    def forward_features(self, x):
-        x = self.stem[0](x) # ⚡ stem_tdnn
+#     def forward_features(self, x):
+#         x = self.stem[0](x) # ⚡ stem_tdnn
 
-        mfa_in = []
-        for i in range(len(self.depths)): # ⚡ 4 -> 3(len(self.depths))
-            x = self.stages[i](x)
-            mfa_in.append(x)
+#         mfa_in = []
+#         for i in range(len(self.depths)): # ⚡ 4 -> 3(len(self.depths))
+#             x = self.stages[i](x)
+#             mfa_in.append(x)
 
-        return mfa_in # ⚡
+#         return mfa_in # ⚡
 
-    def forward(self, x):
-        """
-        Args:
-            x (Tensor): Input tensor of shape (N, C, T).
-        Returns:
-            Tensor: Output tensor of shape (N, C, T).
-        """
-        # x = self.stem[0](x)
+#     def forward(self, x):
+#         """
+#         Args:
+#             x (Tensor): Input tensor of shape (N, C, T).
+#         Returns:
+#             Tensor: Output tensor of shape (N, C, T).
+#         """
+#         # x = self.stem[0](x)
 
-        # x1 = self.stages[0](x)
-        # x2 = self.stages[1](x1)
-        # x3 = self.stages[2](x2)
+#         # x1 = self.stages[0](x)
+#         # x2 = self.stages[1](x1)
+#         # x3 = self.stages[2](x2)
 
-        x = self.forward_features(x) # ⚡
+#         x = self.forward_features(x) # ⚡
 
-        # MFA layer
-        x = torch.cat(x, dim=1)
-        x = self.MFA(x) # Conv1d + LayerNorm_TDNN
+#         # MFA layer
+#         x = torch.cat(x, dim=1)
+#         x = self.MFA(x) # Conv1d + LayerNorm_TDNN
 
-        return x
+#         return x
 
-def MainModel(**kwargs):
-    model = NeXtTDNN_Light(**kwargs)
-    return model
+# def MainModel(**kwargs):
+#     model = NeXtTDNN_Light(**kwargs)
+#     return model
 
